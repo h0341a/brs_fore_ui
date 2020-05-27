ee912439cb2d38836feb896f14085abd170998f1
h0341a <qianaiwuyu@gmail.com>
2020-05-27 13:01:46 +0800

    为侧边导航和AppBar添加内容

--- src/views/AppLayout.vue
+++ src/views/AppLayout.vue
@@ -1,10 +1,27 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      
-    </v-navigation-drawer>
     <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>选项
     </v-app-bar>
+    <v-navigation-drawer width="234" v-model="drawer" app disable-resize-watcher>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">图书推荐</v-list-item-title>
+          <v-list-item-subtitle>为你的书找一些朋友</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <v-container fluid>
         <router-view></router-view>
@@ -18,10 +35,12 @@ export default {
   data() {
     return {
       items: [
-        { title: "Dashboard", icon: "dashboard" },
-        { title: "Account", icon: "account_box" },
-        { title: "Admin", icon: "gavel" }
-      ]
+        { title: "首页", icon: "mdi-view-dashboard" },
+        { title: "联系人", icon: "mdi-account-box-multiple" },
+        { title: "我的推荐", icon: "mdi-book" },
+        { title: "我的收藏", icon: "mdi-book" }
+      ],
+      drawer: true
     };
   }
 };
