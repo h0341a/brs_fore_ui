11d47b7ead090c911161ae4c34ccbac081a1d475
h0341a <qianaiwuyu@gmail.com>
2020-05-27 12:38:19 +0800

    app的页面布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
+
+const AppLayout = ()=>import('../views/AppLayout.vue')
 
   const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'MainLayout',
+    component: AppLayout
   },
   {
     path: '/about',
---
+++ src/views/AppLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      
+    </v-navigation-drawer>
+    <v-app-bar app>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "dashboard" },
+        { title: "Account", icon: "account_box" },
+        { title: "Admin", icon: "gavel" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
